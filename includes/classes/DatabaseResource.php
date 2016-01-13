67030303b1717aa318e6e2d3cab0222767ddd422
Arjo Bruijnes <arjo@weirdwater.net>
2016-01-13 15:26:40 +0100

    Add Database call to static function fromId()

--- includes/classes/DatabaseResource.php
+++ includes/classes/DatabaseResource.php
@@ -9,14 +9,26 @@
 class DatabaseResource
 {
     private $id;
-    private $table;
+
+    public static $table,
+                  $id_collumn;
+
     public static function fromId($id)
     {
         global $db, $hades;
 
         if (is_numeric($id)) {
             try {
-
+                $statement = $db->prepare("
+                    SELECT *
+                    FROM ?
+                    WHERE ? = ?
+                ");
+                $statement->bindParam(1, self::$table, PDO::PARAM_STR);
+                $statement->bindParam(2, self::$id_collumn, PDO::PARAM_STR);
+                $statement->bindParam(3, $id, PDO::PARAM_INT);
+                $statement->execute();
+                $results = $statement->fetch(PDO::FETCH_BOTH);
             }
             catch (Exception $e) {
                 $hades->databaseError($e);
